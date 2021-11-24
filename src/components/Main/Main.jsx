1917223477f302eecd49412f9a96bfe6ce3a8632
leeminewook <63051680+leeminewook@users.noreply.github.com>
2021-11-24 18:18:01 +0900

    [UPDATE] 채팅 버튼 만들기

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import "./Main.css";
+import chattingBtn from "../../assets/img/Mask Group.png";
 const Main = () => {
   return (
     <>
       <div>main</div>
+      <div className="chattingDiv">
+        <img src={chattingBtn} alt="" className="chattingBtn-img" />
+      </div>
     </>
   );
 };
